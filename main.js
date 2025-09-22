f35a926954fbb7e4212a4e1377dc9458ab2575a7
SCM42 <117255815+SCM42@users.noreply.github.com>
2025-09-22 13:18:48 -0400

    Style the grid using JS instead of CSS
    Project prioritizes DOM manipulation so I removed the grid-square width,
    height, and border styling from the stylesheet and styled them directly
    through the DOM

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const parentContainer = document.querySelector("#container");
 
 for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.classList.add("grid-square");
+    const newSquare = document.createElement("div");
+    newSquare.classList.add("grid-square");
+    newSquare.setAttribute("style", "width: 48px; height: 48px; border: 1px solid black");
 
-    parentContainer.appendChild(square);
+    parentContainer.appendChild(newSquare);
 }
