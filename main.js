0e6bca67f828290c9b09af3869da6e0f32399397
SCM42 <117255815+SCM42@users.noreply.github.com>
2025-09-23 00:16:51 -0400

    Add a button that allows user to create new grid
    Commented out loop that created a 16 x 16 grid for testing. New button
    has an event listener that will prompt the user for squares per side.
    Wrote a new function that creates the new grid with a separate callback
    function for calculating the new square sizes. Moved the event listeners
    for drawing over squares into new grid function so that the new grid is
    now responsive.

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 const parentContainer = document.querySelector("#container");
+parentContainer.style = "border: 2px solid black";
+const button = document.querySelector("#btn");
 
-for (let i = 0; i < 256; i++) {
-    const newSquare = document.createElement("div");
-    newSquare.classList.add("grid-square");
-    newSquare.setAttribute("style", "width: 48px; height: 48px; border: 1px solid black");
+button.addEventListener("click", () => {
+    let userInput = parseInt(prompt("Input squares per side for new grid:"));
 
-    parentContainer.appendChild(newSquare);
-}
+    createGrid(userInput, calculateSquareSize(userInput));
+});
 
-const squares = document.querySelectorAll(".grid-square");
+function calculateSquareSize (input) {
+    return newSize = (800 / input) - 2;
+};
 
-squares.forEach((square) => {
+function createGrid(input, size) {
+    const totalSquares = (input * input);
+    
+    for (let i = 0; i < totalSquares; i++) {
+        const newSquare = document.createElement("div");
+        newSquare.classList.add("grid-square");
+        newSquare.setAttribute("style", `width: ${size}px; height: ${size}px; border: 1px solid black`);
+
+        parentContainer.appendChild(newSquare);
+    }
+
+    const squares = document.querySelectorAll(".grid-square");
+
+    squares.forEach((square) => {
     square.addEventListener("mouseenter", () => {
         square.style.background = "grey"
     });
 });
+};
