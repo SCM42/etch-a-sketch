95739eb653b4603215d981927d60d93d06977691
SCM42 <117255815+SCM42@users.noreply.github.com>
2025-09-23 00:45:40 -0400

    Add new function called deleteGrid()
    This function deletes all the children of the parent container before
    calling the functions for creating a new grid.

--- main.js
+++ main.js
@@ -3,31 +3,43 @@ parentContainer.style = "border: 2px solid black";
 const button = document.querySelector("#btn");
 
 button.addEventListener("click", () => {
-    let userInput = parseInt(prompt("Input squares per side for new grid:"));
+  let userInput = parseInt(prompt("Input squares per side for new grid:"));
 
-    createGrid(userInput, calculateSquareSize(userInput));
-});
+  deleteGrid();
+  createNewGrid(userInput, calculateSquareSize(userInput));
+})
 
-function calculateSquareSize (input) {
-    return newSize = (800 / input) - 2;
-};
-
-function createGrid(input, size) {
-    const totalSquares = (input * input);
-    
-    for (let i = 0; i < totalSquares; i++) {
-        const newSquare = document.createElement("div");
-        newSquare.classList.add("grid-square");
-        newSquare.setAttribute("style", `width: ${size}px; height: ${size}px; border: 1px solid black`);
-
-        parentContainer.appendChild(newSquare);
+//#region
+function deleteGrid () {
+    while (parentContainer.firstChild) {
+        parentContainer.removeChild(parentContainer.lastChild);
     }
+}
 
-    const squares = document.querySelectorAll(".grid-square");
+function calculateSquareSize(input) {
+  return 800 / input - 2;
+}
 
-    squares.forEach((square) => {
+function createNewGrid(input, size) {
+  const totalSquares = input * input;
+
+  for (let i = 0; i < totalSquares; i++) {
+    const newSquare = document.createElement("div");
+    newSquare.classList.add("grid-square");
+    newSquare.setAttribute(
+      "style",
+      `width: ${size}px; height: ${size}px; border: 1px solid black`
+    );
+
+    parentContainer.appendChild(newSquare);
+  }
+
+  const squares = document.querySelectorAll(".grid-square");
+
+  squares.forEach((square) => {
     square.addEventListener("mouseenter", () => {
-        square.style.background = "grey"
+      square.style.background = "grey";
     });
-});
-};
+  }); 
+}
+//#endregion
